286376e9ba8e6925244569b92a146af0b9585e20
Matthew Milner <matterhorn103@proton.me>
2025-12-09 23:10:40 +0100

    Spin out code from quanstants

---
+++ src/error.rs
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2025 Matthew Milner <matterhorn103@proton.me>
+// SPDX-License-Identifier: MIT
+
+use std::{error::Error, fmt};
+
+#[derive(Clone, Debug)]
+pub enum SciNumError {
+    Parse(String),
+}
+
+impl fmt::Display for SciNumError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            SciNumError::Parse(string) => write!(f, "Failed to parse: {string}"),
+        }
+    }
+}
+
+impl Error for SciNumError {}
