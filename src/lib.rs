62154356fa2ba3caaede12764ad36117e82b8018
Matthew Milner <matterhorn103@proton.me>
2026-01-02 18:58:21 +0100

    Add rounding mode enum

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@ mod float;
 mod num;
 mod numeric;
 mod bigdecimal;
+mod rounding;
 
 pub use numeric::SciNumeric;
 pub use num::SciNum;
 pub use float::SciFloat;
 pub use bigdecimal::SciBigDecimal;
+pub use rounding::RoundingMode;
